51c271ba7be5a6f82096aa211d7976d500c420a7
Kai-Uwe Humpert <kai-uwe.humpert@infopark.de>
2013-09-17 13:16:39 +0200

    [swf_tasks] Check for longest active worker

--- swf_tasks/swf_tasks.rb
+++ swf_tasks/swf_tasks.rb
@@ -29,8 +29,8 @@ class SwfTasks < Scout::Plugin
         end
   end
 
-  def metric_for_app(app)
-    "#{app}_waiting_tasks"
+  def metric_for_app(app, category = "waiting_tasks")
+    "#{app}_#{category}"
   end
 
   def swf_domain
@@ -58,10 +58,19 @@ class SwfTasks < Scout::Plugin
   def build_report
     open_executions.each do |ex|
       last_event = ex.history_events.reverse_order.first
+      app = app_name_for_task_list(ex.task_list)
       case last_event.event_type
       when "ActivityTaskScheduled"
-        app = app_name_for_task_list(ex.task_list)
         statistics[metric_for_app(app)] += 1
+      when "ActivityTaskStarted"
+        metric = metric_for_app(app, "max_task_duration")
+        event_start = last_event.created_at
+        duration = Time.now - event_start
+        if duration > statistics[metric]
+          statistics[metric] = duration.to_i
+          first_event = ex.history_events.first
+          statistics["#{metric}_name"] = "#{ex.workflow_id}: #{first_event.attributes[:input]}"
+        end
       end
     end
     report(statistics)
